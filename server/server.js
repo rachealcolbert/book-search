dbf478cc03233fbcb983b8fc2781e15006022020
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-07 22:06:50 -0700

    typeDefs creates

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const routes = require("./routes");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
