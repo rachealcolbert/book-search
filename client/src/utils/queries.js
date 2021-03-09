007f0eab90122d5b3b7858e382a382054699edf8
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-08 21:34:04 -0700

    LOGIN_USER added to login form

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,25 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
+export const QUERY_BOOKS = gql`
+  query {
+    books {
+      _id
+      title
+      image
+      link
+      authors
+    }
+  }
+`;
+
+export const QUERY_GET_ME = gql`
+  {
+    me {
+      _id: ID
+      username: String
+      email: String
+      bookCount: Int
+      savedBooks: [Book]
+    }
+  }
+`;
